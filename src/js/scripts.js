0688e1b18cdba55e70936a4a520ea6615634d5f5
Kenneth Au <kenneth.au@apn.com.au>
2016-07-08 14:23:05 +1000

    added BEM css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,21 @@ $(window).resize(fullscreen);
 $(window).scroll(headerParallax);
 
 function fullscreen() {
-	var vBackground = $('.vBackground');
+	var videoBackground = $('.videoBackground');
 	var windowH = $(window).height();
 	var windowW = $(window).width();
 
-	vBackground.width(windowW);
-	vBackground.height(windowH);
+	videoBackground.width(windowW);
+	videoBackground.height(windowH);
 }
 
 function headerParallax() {
 	var st = $(window).scrollTop();
-	var headerScroll = $('.vBackground h1');
+	var headerScroll = $('.videoBackground h1');
 
 	if (st < 500) {
 		headerScroll.css('opacity', 1-st/1000);
-		$('.vBackground-arrow ').css('opacity', 0.5-st/250);
+		$('.videoBackground-arrow ').css('opacity', 0.5-st/250);
 		headerScroll.css({
 			'-webkit-transform' : 'translateY(' + st/7 + '%)',
 			'-ms-transform' : 'translateY(' + st/7 + '%)',
